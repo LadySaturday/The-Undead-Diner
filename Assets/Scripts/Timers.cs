f95d705439c3ce4b6a4c4f874413e36c0aff8fd1
nparson5 <nparson5@my.centennialcollege.ca>
2020-10-01 10:36:25 -0400

    Created Web Version

---
+++ Assets/Scripts/Timers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+public class Timers : MonoBehaviour
+{
+    public int maxTime;
+    public int inc_dec;//increasing or decreasing
+    // Start is called before the first frame update
+    void Start()
+    {
+        animate();   
+    }
+
+    private void animate()
+    {
+        LeanTween.scaleX(gameObject, inc_dec, maxTime);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(gameObject.transform.parent);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
